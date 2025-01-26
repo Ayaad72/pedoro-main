2fa130e81e75a862f2c3ecd464997efa18547c95
Ayaad72 <ayaadalavi@gmail.com>
2025-01-26 21:34:05 +0500

    fixed some wisth issues

--- components/pedronomics.tsx
+++ components/pedronomics.tsx
@@ -1,6 +1,6 @@
 export default function Pedronomics() {
   return (
-    <div className="w-full min-h-[400px] bg-gradient-to-br flex items-center">
+    <div className="max-w-[1440px] mx-auto min-h-[400px] bg-gradient-to-br flex items-center">
       <div className="container mx-auto px-4 md:px-6 lg:px-0 w-[80%] lg:w-full">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center pb-28 lg:pb-16 border-b-2 border-white lg:border-[#41CE84]">
           <h1 className="text-[#41CE84] text-5xl md:text-6xl lg:text-7xl font-bold tracking-tight font-[family-name:var(--font-pixelify-sans)]">
