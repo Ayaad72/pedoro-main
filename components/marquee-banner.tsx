5de1ecba0ac6b7c64898d73bb31fb11c8258dc68
Ayaad72 <ayaadalavi@gmail.com>
2025-01-30 19:38:28 +0500

    finalized changes

--- components/marquee-banner.tsx
+++ components/marquee-banner.tsx
@@ -11,7 +11,7 @@ export default function MarqueegBanner() {
         x
       </span>
       <span className="inline-flex items-center text-[#7F1911] text-xl font-medium px-4">
-        fsdfsdf EPIC
+        saasdafa EPIC
       </span>
       <span className="inline-flex items-center text-[#7F1911] text-xl font-medium px-4">
         x
